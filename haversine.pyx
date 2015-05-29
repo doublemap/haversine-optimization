6a7dd23315cc01c08de0922d370f7c7a7f9b8b91
Eric Jiang <eric@doublemap.com>
2015-05-29 10:57:26 -0400

    Add c types for variables

--- haversine.pyx
+++ haversine.pyx
@@ -1,9 +1,13 @@
 from libc.math cimport sin, cos, acos
 
 
-def haversine(coord1, coord2):
+def haversine(tuple coord1, tuple coord2):
     """Given two (lat, lng) tuples, returns the distance between them in
     meters."""
+    cdef double lat1
+    cdef double lng1
+    cdef double lat2
+    cdef double lng2
     lat1, lng1 = coord1
     lat2, lng2 = coord2
 
@@ -11,6 +15,13 @@ def haversine(coord1, coord2):
         raise ValueError("Invalid latitude (should be between +/- 90)")
     if lng1 > 180 or lng1 < -180 or lng2 > 180 or lng2 < -180:
         raise ValueError("Invalid longitude (should be between +/- 180)")
+
+    cdef double ph1
+    cdef double ph2
+    cdef double theta1
+    cdef double theta2
+    cdef double c
+    cdef double arc
 
     phi1 = (90.0 - lat1) * 0.0174532925
     phi2 = (90.0 - lat2) * 0.0174532925
